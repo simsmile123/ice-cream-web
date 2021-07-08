8ed366b8aef3cda2472a349bc52a6fceb1809725
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2021-07-08 16:45:19 -0400

    im dying inside

---
+++ pages/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+const logo = require("./ice-cream-logo-with-text.png");
+
+const NavBar = () => {
+  return (
+    <nav>
+      <h2 className="pl-8 ">ICE CREAM</h2>
+    </nav>
+  );
+};
+
+export default NavBar;
